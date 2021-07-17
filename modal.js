76e56ec8e5fc0ebd34a5fbb9e2673993605c92ea
EmbitiousDev <49172724+EmbitiousDev@users.noreply.github.com>
2021-07-17 10:11:40 +0200

    first step of validating form data

--- modal.js
+++ modal.js
@@ -1,10 +1,10 @@
 function editNav() {
-  var x = document.getElementById('myTopnav');
-  if (x.className === 'topnav') {
-    x.className += ' responsive';
-  } else {
-    x.className = 'topnav';
-  }
+	var x = document.getElementById('myTopnav');
+	if (x.className === 'topnav') {
+    	x.className += ' responsive';
+  	} else {
+    	x.className = 'topnav';
+  	}
 }
 
 // DOM Elements
@@ -17,11 +17,23 @@ modalBtn.forEach((btn) => btn.addEventListener('click', launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = 'block';
+	modalbg.style.display = 'block';
 }
 
 // close modal
 const modalCloseButton = document.getElementById('closeModal');
 modalCloseButton.addEventListener('click', () => {
-  modalbg.style.display = 'none';
+	modalbg.style.display = 'none';
 });
+
+// validate content
+function validate() {
+	let formFirstName = document.forms['book']['first'].value;
+	let formLastName = document.forms['book']['last'].value;
+	let formEmail = document.forms['book']['email'].value;
+	let formBirthDate = document.forms['book']['birthdate'].value;
+	let formPreviousConstestAmount = document.forms['book']['quantity'].value;
+	let formPreviousCities = document.forms['book']['location'].value; 
+	let formAcceptConditions = document.forms['book']['checkbox1'].value;
+	let formSubscribeNewsletter = document.forms['book']['checkbox2'].value;
+}
