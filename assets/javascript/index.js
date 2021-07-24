fda9e565dbd44f875eac9af3813bb49c26ff13d9
EmbitiousDev <49172724+EmbitiousDev@users.noreply.github.com>
2021-07-24 18:44:33 +0200

    feature(form): finish the form, check data, retain data if invalid, and confirm submission when valid, fix #2, fix #4

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,11 +1,15 @@
 import {openModal} from './modules/openModal.js';
 import {closeModal} from './modules/closeModal.js';
-// import validateForm from './modules/validateForm.js';
+import {validateForm} from './modules/validateForm.js';
 
 const modalbg = document.querySelector('.bground');
 const modalBtn = document.querySelectorAll('.modal-btn');
+const bookForm = document.forms['book'];
 const modalCloseButton = document.getElementById('closeModal');
+const submitFormButton = document.getElementById('submit-form-btn');
 
 modalBtn.forEach((btn) => btn.addEventListener('click', () => {openModal(modalbg)}));
 
 modalCloseButton.addEventListener('click', () => {closeModal(modalbg)});
+
+submitFormButton.addEventListener('click', () => {validateForm(bookForm)});
--- assets/javascript/modules/validateForm.js
+++ assets/javascript/modules/validateForm.js
@@ -1,16 +1,36 @@
-export function validateForm() {
-    const submitFormButton = document.getElementById('btn-submit');
+function validateForm(bookForm) {
 
-    submitFormButton.addEventListener('click', () => {
+    let previousConstestAmount = 0;
+    let previousCities = [];
+    let isPreviousCitiesValid = true;
 
-        let isFirstNameValid = document.forms['book']['first'].validity.valid;
-	    let isLastNameValid = document.getElementById('last').validity.valid;
-	    let isEmailValid = document.getElementById('email').validity.valid;
-	    let isBirthDateValid = document.getElementById('birthdate').validity.valid;
-        let isAcceptConditionsValid = document.getElementById('checkbox1').validity.valid;
+    let isFirstNameValid = bookForm['first'].validity.valid;
+	let isLastNameValid = bookForm['last'].validity.valid;
+	let isEmailValid = bookForm['email'].validity.valid;
+	let isBirthDateValid = bookForm['birthdate'].validity.valid;
+    let isPreviousConstestAmountValid = bookForm['quantity'].validity.valid;
+    let isNewsletterValid = bookForm['checkbox1'].checked;
+    let isAcceptConditionsValid = bookForm['checkbox2'].checked;
 
-        if ( !isFirstNameValid && !isLastNameValid && !isEmailValid && !isBirthDateValid && !isAcceptConditionsValid) {
-            submitFormButton.preventDefault();
-        }
-    })
+    for(let i = 0; i < 6; i++){
+        previousCities.push(bookForm['location'+i].checked);
+    }
+
+    if (isPreviousConstestAmountValid) {
+        previousConstestAmount = Number(bookForm['quantity'].value);
+    }
+
+    if (previousConstestAmount > 0) {
+        isPreviousCitiesValid = previousCities.reduce((a, b) => a || b);
+    }
+
+    if (isFirstNameValid && isLastNameValid && isEmailValid && isBirthDateValid && isPreviousConstestAmountValid && isPreviousCitiesValid && isNewsletterValid && isAcceptConditionsValid) {
+        bookForm.submit();
+        alert('Votre réservation a bien été prise en compte, merci!');
+    }
+    else {
+        bookForm.addEventListener('submit', (evt) => {evt.preventDefault()});
+    }
 }
+
+export {validateForm};
